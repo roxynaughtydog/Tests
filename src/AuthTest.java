6643509dfd9dea9823f200f5b5968d74031c2465
Nick Ukolov <ukolovnikita96@gmail.com>
2022-10-19 17:03:09 +0500

    Initial commit

---
+++ src/AuthTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class AuthTest {
+    @Test
+    public void successrwrs () {
+
+
+
+    }
--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,17 @@
-public class Test {
+package examples;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthTest {
+
+    @Test
+    void test1() {
+        step("Go to the Website: ...");
+        step("Click Account ");
+        step("Click Companies");
+        step("Click +");
+        step("Fill all fields with valid data");
+        step("Click Save ");
+    }
 
 }
